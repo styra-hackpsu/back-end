88a24a58807c515d2bb6ac92fa8bdfbc14a911e6
zyrch <rupanshu19475@iiitd.ac.in>
2021-03-21 04:44:17 +0530

    reflect new api change

--- services/api.py
+++ services/api.py
@@ -59,25 +59,34 @@ def getKeywords(link):
         word['score'] = 1/word['score']
         
         # Testing
-        
-        if word['ngram'] in totol_keywords:
-            totol_keywords[word['ngram']] += word['score']
-        else:
-            totol_keywords[word['ngram']] = word['score']
+
+        totol_keywords[word['ngram']] = word['score']
     
     total_keywords = {k: v for k, v in sorted(totol_keywords.items(), key=lambda item: item[1], reverse=True)}
     
     return total_keywords 
 
 
-def detect_change(history_all: list, history_just: list) -> bool:
+def detect_change(history_all: dict, history_just: dict) -> bool:
 
     def getFullHistoryKeyword(history, count):
 
-        top_count = {}
+        top_all = {}
+        for link in history:
+            keywords = history[link]
+
+            for word in keywords:
+
+                if word in top_all:
+                    top_all[word] += keywords[word]
+                else:
+                    top_all[word] = keywords[word]
         
-        for key in total_keywords.keys():
-            top_count[key] = total_keywords[key]
+        top_all = {k: v for k, v in sorted(top_all.items(), key=lambda item: item[1], reverse=True)}
+       
+        top_count = {}
+        for key in top_all.keys():
+            top_count[key] = top_all[key]
             
             if len(top_count) == count:
                 break
@@ -90,10 +99,11 @@ def detect_change(history_all: list, history_just: list) -> bool:
     total = 0
     for topic in keywords_just:
         if topic in keywords_all:
-            print(topic)
+            #print(topic)
             count+=keywords_just[topic]
         total += keywords_just[topic]
 
     score = count/total
-    
+   
     return score > THRESHOLD
+
