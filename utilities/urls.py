0cbf723d913fd5cee44c649b90ae6e8a296c5494
watch24hrs-iiitd <divyansh19464@iiitd.ac.in>
2021-03-21 14:15:54 +0530

    Add: update response requests

--- utilities/urls.py
+++ utilities/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('face-detect/', views.FaceDetect.as_view(), name='face_detect'), # POST PARAMS: ENCODED IMAGE, CHOICE=1
-    path('change-detect/', views.ChangeDetect.as_view(), name='change_detect'), # POST PARAMS: URL (TAB)
+    path('change-detect/', views.ChangeDetect.as_view(), name='change_detect'), # POST PARAMS: URL (of TAB)
     path('user-emotions/', views.UserEmotionViewSet.as_view({'get': 'list'})),
-    path('user-keywords/', views.UserKeywordViewSet.as_view({'get': 'list'}))
+    path('user-keywords/', views.UserKeywordViewSet.as_view({'get': 'list'})),
+    path('face-detect-response/<int:pk>/<str:response>/', views.update_user_emotion_response, name='update_user_emotion_response'),
+    path('change-detect-response/<int:pk>/<str:response>/', views.update_user_keyword_response, name='update_user_keyword_response')
 ]
